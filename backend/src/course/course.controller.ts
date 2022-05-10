959f88535b94503f6b09d31825b441b96815d186
pspapleng <chayanin3343@gmail.com>
2022-05-10 17:52:17 +0700

    feat: find all course

--- backend/src/course/course.controller.ts
+++ backend/src/course/course.controller.ts
@@ -1,32 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { CourseService } from './course.service';
 
 @Controller('course')
 export class CourseController {
   constructor(private readonly courseService: CourseService) {}
 
-  // @Post()
-  // create(@Body() createTodoDto: CreateTodoDto) {
-  //   return this.todoService.create(createTodoDto);
-  // }
-
-  // @Get()
-  // findAll(@Query() query: QueryTodoDto) {
-  //   return this.todoService.findAll(query);
-  // }
-
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.todoService.findOne(id);
-  // }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
-  //   return this.todoService.update(id, updateTodoDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.todoService.remove(id);
-  // }
+  @Get()
+  findAll() {
+    return this.courseService.findAll();
+  }
 }
--- backend/src/course/course.service.ts
+++ backend/src/course/course.service.ts
@@ -5,35 +5,7 @@ import { CourseRepository } from './../pkg/dal/course/course.repository';
 export class CourseService {
   constructor(private courseRepository: CourseRepository) {}
 
-  // create(createTodoDto: CreateTodoDto) {
-  //   const newTodo = this.todoRepository.create({
-  //     message: createTodoDto.message,
-  //     status: TODO_STATUS.Todo,
-  //   });
-  //   return this.todoRepository.save(newTodo);
-  // }
-
-  // findAll(query: QueryTodoDto) {
-  //   return this.todoRepository.find({
-  //     take: query.limit,
-  //     skip: query.skip,
-  //   });
-  // }
-
-  // findOne(id: string) {
-  //   return this.todoRepository.findOne({ id: id });
-  // }
-
-  // async update(id: string, updateTodoDto: UpdateTodoDto) {
-  //   const todo = await this.findOne(id);
-  //   if (updateTodoDto.message) todo.message = updateTodoDto.message;
-  //   if (updateTodoDto.status) todo.status = updateTodoDto.status;
-
-  //   return this.todoRepository.save(todo);
-  // }
-
-  // async remove(id: string) {
-  //   const todo = await this.findOne(id);
-  //   return this.todoRepository.softRemove(todo);
-  // }
+  findAll() {
+    return this.courseRepository.find();
+  }
 }
